8d489cd6eedb5735f034d88b583b826728df6f6a
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-21 09:46:57 +0900

    Add files via upload

---
+++ mvkdue.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/update/240313/bg_04_classchange_loop.mp4#t=0.1";
+        document.getElementById('vd').load();
+    }
+});
+
